4d8a4aeddb1163a231662ea7a5a9c6d1db435f35
Diego Burlando <diego@dmm888.com>
2018-01-22 15:25:34 +0100

    Checked functionality in netbeans 8.2

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,21 +3,25 @@ import org.springframework.boot.autoconfigure.*;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.mongo.MongoRepositoriesAutoConfiguration;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.ui.ModelMap;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
+/**
+ *
+ * @author Diego
+ */
 @ComponentScan(basePackages = "controllers")
 @EnableAutoConfiguration(exclude={MongoAutoConfiguration.class,MongoDataAutoConfiguration.class,MongoRepositoriesAutoConfiguration.class})
 
-public class Application extends SpringBootServletInitializer {   
+public class Application extends SpringBootServletInitializer {
 
-
-
- @Override
+    /**
+     *  
+     * @param application
+     * @return
+     */
+    @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Application.class);
     }
--- src/main/java/controllers/TestController.java
+++ src/main/java/controllers/TestController.java
@@ -1,24 +1,12 @@
-
 package controllers;
 import org.springframework.web.bind.annotation.*;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
-import com.mongodb.client.MongoDatabase;
-import java.util.ArrayList;
-import java.util.List;
-import com.google.gson.Gson;
-import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
